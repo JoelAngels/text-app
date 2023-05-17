93e23b82528c00229f29b8846ab5d9e6e561242b
Joel Angel <joelbaraka4325@gmail.com>
2023-05-17 17:34:23 +0300

    customed component initiated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,22 @@ import { StyleSheet, Image } from "react-native";
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import Tweet from "../../components/Tweet";
+import tweets from "../../assets/data/tweets";
+
+const tweet = tweets[0];
 
 export default function TabOneScreen() {
   return (
-    <View>
+    <View style={styles.page}>
       <Tweet />
       <Tweet />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderColor: "lightgrey",
+    backgroundColor: "white",
   },
 
   userImage: {
