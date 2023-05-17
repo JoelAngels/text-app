c87e033e2f088be85229b2ce6d6f61b47574c7d9
Joel Angel <joelbaraka4325@gmail.com>
2023-05-17 16:07:20 +0300

    rendering my first message

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import tweets from "../../assets/data/tweets";
+
+const tweet = tweets[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image src={tweet.user.image} style={styles.userImage} />
+      <View style={styles.mainContainer}>
+        <Text>{tweet.user.name}</Text>
+        <Text>{tweet.content}</Text>
+      </View>
     </View>
   );
 }
@@ -16,16 +21,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  mainContainer: {
+    marginLeft: 10,
   },
 });
