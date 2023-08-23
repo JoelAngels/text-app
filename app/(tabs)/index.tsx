c3043f505b45b16e9ef0b91944db22524f56f72c
Joel Angel <joelbaraka4325@gmail.com>
2023-08-23 15:14:05 +0300

    worked on the floating icon for the twitter app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, FlatList, View } from "react-native";
+import { StyleSheet, FlatList, View, Pressable, Text } from "react-native";
 import Tweet from "../../components/Tweet";
 import tweets from "../../assets/data/tweets";
+import { Entypo } from "@expo/vector-icons";
 
 export default function TabOneScreen() {
   return (
@@ -9,6 +10,10 @@ export default function TabOneScreen() {
         data={tweets}
         renderItem={({ item }) => <Tweet tweet={item} />}
       />
+
+      <Pressable style={styles.floatingButton}>
+        <Entypo name="plus" size={24} color="white"></Entypo>
+      </Pressable>
     </View>
   );
 }
@@ -17,5 +22,17 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: "white",
+  },
+  floatingButton: {
+    backgroundColor: "#1C9BF0",
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 15,
+    bottom: 15,
+    cursor: "pointer",
   },
 });
---
+++ app/new-tweet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+
+export default function Newtweet() {
+  return (
+    <View>
+      <Text>Create new Tweet</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
