a894576c8ecb8219ac46c1fec5d557ca8a7b15ad
Joel Angel <joelbaraka4325@gmail.com>
2023-08-28 17:40:01 +0300

    drawer navigation config

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -9,17 +9,39 @@ const DrawerNavigator = createDrawerNavigator().Navigator;
 
 const Drawer = withLayoutContext(DrawerNavigator);
 
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: "(tabs)",
 };
 
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <Text style={{ alignSelf: "center", fontSize: 20 }}>Joel</Text>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
 export default function DrawerLayout() {
   return (
-    <Drawer>
+    <Drawer drawerContent={(props) => <CustomDrawerContent {...props} />}>
       <Drawer.Screen
         name="(tabs)"
         options={{ headerShown: false, title: "Home" }}
+      />
+      <Drawer.Screen
+        name="bookmarks"
+        options={{ headerShown: false, title: "Bookmarks" }}
+      />
+      <Drawer.Screen
+        name="twitter-blue"
+        options={{ headerShown: false, title: "Twitter Blue" }}
       />
     </Drawer>
   );
